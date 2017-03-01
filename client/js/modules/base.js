bf0d7a4558944571eeb75def69124baf049c6f0b
Tamara Bazko <tamarabazko@gmail.com>
2017-03-01 12:14:09 +0100

    Removing some unneccessary files. Putting form validator to base.js

--- client/js/modules/base.js
+++ client/js/modules/base.js
@@ -1,22 +1,26 @@
-  var readCookie = require('../components/utils/utils').readCookie;
-  var Menu = require('../components/Menu');
-  var Counter = require('../components/Counter');
+var readCookie = require('../components/utils/utils').readCookie;
+var Menu = require('../components/Menu');
+var Counter = require('../components/Counter');
+var FormValidator = require('../components/FormValidator');
 
-  if(readCookie('logged')) {
-    var userId = readCookie('logged');
-    window.socket = io();
-    var socket = window.socket;
-    var counter;
+if(readCookie('logged')) {
+  var userId = readCookie('logged');
+  window.socket = io();
+  var socket = window.socket;
+  var counter;
 
-    socket.on('newSwapRequest', function(data) {
-      if(!counter) {
-        counter = new Counter();
-      }
-      counter.update();
-    });
+  socket.on('newSwapRequest', function(data) {
+    if(!counter) {
+      counter = new Counter();
+    }
+    counter.update();
+  });
 
-    socket.emit('joinRoom', userId);
-  }
+  socket.emit('joinRoom', userId);
+}
 
-  var menu = new Menu('js-menu-wrapper', 'js-menu-trigger');
-  menu.init();
+var form = new FormValidator();
+form.init();
+
+var menu = new Menu('js-menu-wrapper', 'js-menu-trigger');
+menu.init();
